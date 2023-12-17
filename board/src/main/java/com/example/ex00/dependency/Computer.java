03ce750638c1736c00567ab971ca1d54a93ad358
Song-Yunseop <99631105+Song-Yunseop@users.noreply.github.com>
2023-12-18 01:37:33 +0900

    feat : 의존성 주입

---
+++ board/src/main/java/com/example/ex00/dependency/Computer.java
@@ -0,0 +1,13 @@
+package com.example.ex00.dependency;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Computer {
+	
+	private int ram;
+	
+}
